b1eaf4246e213382e98f97bd03667611be123b26
Ryan Wood <ryan.wood@electroroute.co.jp>
2023-10-30 15:12:45 +0900

    Added capabilities

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 resource "aws_cloudformation_stack" "access" {
   name         = var.alphaus_cf_name
   template_url = "https://cover-cloudformation-templates.s3.ap-northeast-1.amazonaws.com/coverapiaccess-v1.yml"
+  capabilities = ["CAPABILITY_NAMED_IAM"]
   on_failure   = "ROLLBACK"
   parameters = {
     Principal  = var.alphaus_principal
