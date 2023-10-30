e366f6412334e29b67f61aa4b098f0d81c94d4b4
Ryan Wood <ryan.wood@electroroute.co.jp>
2023-10-30 12:22:40 +0900

    Removed redundant module components

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,9 @@
-output "alphaus_acct_access_arn" {
-  value       = aws_iam_role.alphaus_acct_access_role.arn
-  description = "ARN of the role providing Alphaus OCTO with AWS access"
+output "alphaus_access_cf_template_id" {
+  value       = aws_cloudformation_stack.access.id
+  description = "ID of the CloudFormation template used to create the Alphaus access"
 }
 
-output "cloud_saver_tag_manager_arn" {
-  value       = aws_iam_policy.cloud_saver_tag_manager_policy.arn
-  description = "ARN of the tag-manager policy document used to configure the Cloud Saver role"
-}
-
-output "cloud_saver_tag_manager2_arn" {
-  value       = aws_iam_policy.cloud_saver_tag_manager_policy2.arn
-  description = "ARN of the second tag-manager policy document used to configure the Cloud Saver role"
-}
-
-output "cloud_saver_base_arn" {
-  value       = aws_iam_policy.cloud_saver_base_policy.arn
-  description = "ARN of the base policy document used to configure the Cloud Saver role"
-}
-
-output "cloud_saver_base2_arn" {
-  value       = aws_iam_policy.cloud_saver_base_policy2.arn
-  description = "ARN of the second base policy document used to configure the Cloud Saver role"
-}
-
-output "cloud_saver_base3_arn" {
-  value       = aws_iam_policy.cloud_saver_base_policy3.arn
-  description = "ARN of the third base policy document used to configure the Cloud Saver role"
-}
-
-output "cloud_saver_access_arn" {
-  value       = aws_iam_role.cloud_saver_role.arn
-  description = "ARN of the role created to provide Cloud Saver with AWS access"
+output "alphaus_access_cf_template_outputs" {
+  value       = aws_cloudformation_stack.access.outputs
+  description = "Outputs of the CloudFormation template used to create the Alphaus access"
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "account_id" {
-  type        = string
-  description = "The AWS account ID to which OCTO should be given access."
-}
-
 variable "alphaus_principal" {
   type        = string
   description = "The Alphaus account that will have access to your account. Do not change."
@@ -17,16 +12,4 @@ variable "alphaus_cf_name" {
   type        = string
   default     = "AlphausAccountAccess"
   description = "What to call the Alphaus CloudFormation template"
-}
-
-variable "alphaus_enable_ec2_management" {
-  type        = bool
-  default     = false
-  description = "Whether or not EC2 management should be enabled (will allow OCTO to start/stop/terminate EC2 instances)"
-}
-
-variable "alphaus_allow_self_policy_update" {
-  type        = bool
-  default     = false
-  description = "Whether or not OCTO can update its own access policy. Allows it to handle its own updates."
 }
